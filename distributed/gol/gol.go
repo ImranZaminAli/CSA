c30da55f7769a783d9b6865488484f0b90307748
ImranZaminAli <izaminali@gmail.com>
2021-11-22 17:04:48 +0000

    added distrubuted folder

---
+++ distributed/gol/gol.go
@@ -0,0 +1,40 @@
+package gol
+
+// Params provides the details of how to run the Game of Life and which image to load.
+type Params struct {
+	Turns       int
+	Threads     int
+	ImageWidth  int
+	ImageHeight int
+}
+
+// Run starts the processing of Game of Life. It should initialise channels and goroutines.
+func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
+
+	//	TODO: Put the missing channels in here.
+
+	ioCommand := make(chan ioCommand)
+	ioIdle := make(chan bool)
+	ioFilename := make(chan string)
+	ioInput := make(chan uint8)
+	ioOutput := make(chan uint8)
+	ioChannels := ioChannels{
+		command:  ioCommand,
+		idle:     ioIdle,
+		filename: ioFilename,
+		output:   ioOutput,
+		input:    ioInput,
+	}
+	go startIo(p, ioChannels)
+
+	distributorChannels := distributorChannels{
+		events:     events,
+		ioCommand:  ioCommand,
+		ioIdle:     ioIdle,
+		ioFilename: ioFilename,
+		ioOutput:   ioOutput,
+		ioInput:    ioInput,
+		keyPresses: keyPresses,
+	}
+	distributor(p, distributorChannels)
+}
